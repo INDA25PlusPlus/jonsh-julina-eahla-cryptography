5c073dab05e54476e26ea283ea7737fa78c0a14f
JulinaSavela <julina.savela@outlook.com>
2025-11-11 12:50:27 +0100

    encrypt_file func skeleton

--- cryptography/src/bin/client.rs
+++ cryptography/src/bin/client.rs
@@ -1,8 +1,26 @@
 use std::net::TcpStream;
 use std::io::{Read, Write};
 
-fn main() -> std::io::Result<()> {
+use aes_gcm::aes::cipher;
+use aes_gcm::{
+    aead::{Aead, AeadCore, KeyInit, OsRng},
+    Aes256Gcm, Nonce, Key // Or `Aes128Gcm`
+};
+use std::fs;
 
+
+
+fn encrypt_file(cipher: &Aes256Gcm, file_path: &str) {
+
+    
+
+}
+
+
+
+
+
+fn main() -> std::io::Result<()> {
 
     let mut stream = match TcpStream::connect("127.0.0.1:8080") {
 
@@ -20,6 +38,12 @@ fn main() -> std::io::Result<()> {
 
     let msg_package = stream.read(&mut [0; 128])?;
     println!("{}", msg_package);
+
+    // create encryption key -- same used througout
+    let key =  Aes256Gcm::generate_key(OsRng);
+    let cipher = Aes256Gcm::new(&key);
+
+    
 
     Ok(())
 
