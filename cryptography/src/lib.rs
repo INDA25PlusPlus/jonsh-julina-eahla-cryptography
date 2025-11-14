1eaf9e0510a8713c602baaea8e849507722f611e
Ellinor <ellinorkahlander@gmail.com>
2025-11-14 03:35:50 +0100

    less buggy merkle code

--- cryptography/src/lib.rs
+++ cryptography/src/lib.rs
@@ -1,9 +1,8 @@
 use std::collections::HashMap;
-use sha2::{Digest, Sha256, digest};
-use digest::generic_array::GenericArray;
+use sha2::{Digest, Sha256};
 
 pub struct MerkleTreeNode {
-    pub hash: GenericArray<u8, <Sha256 as Digest>::OutputSizes>,
+    pub hash: [u8; 32],
     pub left_most_index_covered: u64,
     pub right_most_index_covered: u64,
     pub left_child: Option<Box<MerkleTreeNode>>,
@@ -14,42 +13,53 @@ pub struct MerkleTreeNode {
 impl MerkleTreeNode {
     pub fn new(left_index_covered: u64, right_index_covered: u64) -> Self {
         MerkleTreeNode {
-          hash: Sha256::digest(b""),
-          left_most_index_covered: left_index_covered,
-          right_most_index_covered: right_index_covered,
+            hash: Sha256::digest(b"").into(),
+            left_most_index_covered: left_index_covered,
+            right_most_index_covered: right_index_covered,
+            left_child: None,
+            right_child: None,
+            encrypted_data: None,
         }
     }
 
     pub fn new_leaf_node(encrypted_data: Vec<u8>, left_index_covered: u64) -> Self {
         MerkleTreeNode {
-            hash: Sha256::digest(&encrypted_data),
+            hash: Sha256::digest(&encrypted_data).into(),
             left_most_index_covered: left_index_covered,
             right_most_index_covered: left_index_covered,
-            encrypted_data: encrypted_data,
+            encrypted_data: Some(encrypted_data),
+            left_child: None,
+            right_child: None,
         }
     }
 
-    pub fn go_down_and_make_leaf_node(&mut self, to_pos: u64, encrypted_data: Vec<u8>) -> MerkleTreeNode
+    pub fn go_down_and_make_leaf_node(&mut self, to_pos: u64, encrypted_data: Vec<u8>)
     {
         if (self.right_most_index_covered - self.left_most_index_covered == 1) {
             // kids are leaves
             if let Some(ref mut child) = self.left_child {
-                self.right_child = MerkleTreeNode::new_leaf_node(
+                self.right_child = Some(Box::new(MerkleTreeNode::new_leaf_node(
                     encrypted_data,
                     child.right_most_index_covered + 1
-                )
+                )));
 
-                self.hash = Sha256::Digest(self.left_child.hash + self.right_child.hash);
+                let mut hasher = Sha256::new();
+                hasher.update(&self.left_child.as_ref().unwrap().hash);
+                hasher.update(&self.right_child.as_ref().unwrap().hash);
+
+                self.hash = hasher.finalize().into();
             } else {
-                self.left_child = MerkleTreeNode::new_leaf_node(
+                self.left_child = Some(Box::new(MerkleTreeNode::new_leaf_node(
                     encrypted_data,
-                    child.right_most_index_covered + 1
-                )
+                    self.left_child.as_ref().unwrap().right_most_index_covered + 1
+                )));
 
-                self.hash = Sha256::Digest(self.left_child.hash + self.left_child.hash);
+                let mut hasher = Sha256::new();
+                hasher.update(&self.left_child.as_ref().unwrap().hash);
+                hasher.update(&self.left_child.as_ref().unwrap().hash);
+
+                self.hash = hasher.finalize().into();
             }
-
-            self
         }
         else {
             if let Some(ref mut child) = self.left_child {
@@ -60,32 +70,40 @@ impl MerkleTreeNode {
                         child.go_down_and_make_leaf_node(to_pos, encrypted_data);
                     } else {
                         // skapa right
-                        self.right_child = MerkleTreeNode::new(
+                        self.right_child = Some(Box::new(MerkleTreeNode::new(
                             child.right_most_index_covered + 1,
                             self.right_most_index_covered
-                        );
+                        )));
 
-                        self.right_child.go_down_and_make_leaf_node(to_pos, encrypted_data);
+                        self.right_child.as_mut().unwrap().go_down_and_make_leaf_node(to_pos, encrypted_data);
                     }
                 }
             } else {
                 // skapa left
-                self.right_child = MerkleTreeNode::new(
+                self.right_child = Some(Box::new(MerkleTreeNode::new(
                     self.left_most_index_covered,
                     self.left_most_index_covered + ((self.right_most_index_covered + 1 - self.left_most_index_covered) / 2)
-                );
+                )));
 
-                self.left_child.go_down_and_make_leaf_node(to_pos, encrypted_data);
+                self.left_child.as_mut().unwrap().go_down_and_make_leaf_node(to_pos, encrypted_data);
             }
 
             if let Some(ref mut child) = self.right_child {
-                self.hash = Sha256::Digest(self.left_child.hash + child.hash);
-            } else {
-                self.hash = Sha256::Digest(self.left_child.hash + self.left_child.hash);
-            }
+                let mut hasher = Sha256::new();
+                hasher.update(&self.left_child.as_ref().unwrap().hash);
+                hasher.update(child.hash);
 
-            self
+                self.hash = hasher.finalize().into();
+            } else {
+                let mut hasher = Sha256::new();
+                hasher.update(&self.left_child.as_ref().unwrap().hash);
+                hasher.update(&self.left_child.as_ref().unwrap().hash);
+
+                self.hash = hasher.finalize().into();
+            }
         }
+
+        ()
     }
 }
 
@@ -117,25 +135,27 @@ impl MerkleTree {
             self.leaf_nodes_count = self.leaf_nodes_count + 1;
 
             self.file_id_to_leaf_id.insert(file_id, self.leaf_nodes_count - 1);
-
-            self.root.as_ref().unwrap()
         } else {
             // if 2 potens uppdatera rot
-            if ((leaf_count & (leaf_count - 1)) == 0) {
-                let new_root = MerkleTreeNode::new(0, self.root.right_most_index_covered * 2 + 1);
-                new_root.left_child = self.root;
+            let old_root = self.root.take().unwrap();
 
-                self.root = new_root;
+            if ((leaf_count & (leaf_count - 1)) == 0) {
+                let mut new_root = MerkleTreeNode::new(0, old_root.right_most_index_covered * 2 + 1);
+                new_root.left_child = Some(Box::new(old_root));
+
+                self.root = Some(new_root);
             }
 
-            self.root.go_down_and_make_leaf_node(self.leaf_nodes_count, encrypted_data);
+            self.root.as_mut().unwrap().go_down_and_make_leaf_node(self.leaf_nodes_count, encrypted_data);
         }
+
+        self.root.as_ref().unwrap()
     }
 
     pub fn get_leaf_id_from_file_id(&self, file_id: u64) -> Option<u64>
     {
-        if let Some(ref mut leaf_id) = self.file_id_to_leaf_id.get(&file_id) {
-            Some(leaf_id)
+        if let Some(leaf_id) = self.file_id_to_leaf_id.get(&file_id) {
+            Some(*leaf_id)
         } else {
             println!("Invlaid file ID.");
             None
